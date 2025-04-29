b0cd23818bc23ea0ab0556dfab2a4e3b2a86fcac
tanerengiiin <tanerengin4444@gmail.com>
2025-04-29 19:41:23 +0300

    feat: Complete Next.js Dashboard Course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import { Metadata } from "next";
+import React from "react";
+export const metadata: Metadata = {
+  title: "Customers",
+};
 const Page = () => {
-  return (
-    <div>Customers Page</div>
-  )
-}
+  return <div>Customers Page</div>;
+};
 
-export default Page
+export default Page;
